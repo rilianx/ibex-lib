e1b02e2d3570f3b879f601983fcd5e2f8b7a076e
iaraya <ignacio.araya@pucv.cl>
2018-04-26 15:34:53 -0300

    cont

--- plugins/pso/src/strategy/ibex_BufferPSO.cpp
+++ plugins/pso/src/strategy/ibex_BufferPSO.cpp
@@ -39,53 +39,53 @@ namespace ibex {
 
 	Cell* BufferPSO::top(double current_ub) const{
 
+		for(int i=0;i<popsize; i++){
+		  if(!particles[i]){
+			//TODO: seleccionar nodos prometedores de zonas altas del árbol
+			Cell* c = tree->minlb(tree->root);
+			particles[i] = new Part(c->box.random());
+		  }
+		//  cout << i << ":" << -particles[i]->fitness(current_ub) << endl;
+		}
 
-    for(int i=0;i<popsize; i++){
-      if(!particles[i]){
-        //TODO: seleccionar nodos prometedores de zonas altas del árbol
-        Cell* c = tree->minlb(tree->root);
-        particles[i] = new Part(c->box.random());
-      }
-    //  cout << i << ":" << -particles[i]->fitness(current_ub) << endl;
-    }
+		/*
+			Seleccionar 2 partículas
+			h = p2 + rand(0.0,2.0)*(p1-p2)
+			Si h es box-feasible y h>p2, p2=h
+			Retornar mejor de las 3 partículas
+			*/
 
-    /*
-		Seleccionar 2 partículas
-		h = p2 + rand(0.0,2.0)*(p1-p2)
-		Si h es box-feasible y h>p2, p2=h
-		Retornar mejor de las 3 partículas
-		*/
+		int i1 = rand()%popsize;
+		int i2 = rand()%popsize;
+		while(i2==i1) i2=rand()%popsize;
+	  //
 
-    int i1 = rand()%popsize;
-    int i2 = rand()%popsize;
-    while(i2==i1) i2=rand()%popsize;
-  //  cout << i1 << "," << i2 << endl;
+		Part* p1=particles[i1];
+		Part* p2=particles[i2];
+		if(p1->fitness(current_ub) < p1->fitness(current_ub)){
+		  p1=p2;
+		  p2=particles[i1];
+		}
+		double r = (2.0*rand())/RAND_MAX;
+		Vector h = p2->position + r*(p1->position-p2->position);
 
-    Part* p1=particles[i1];
-    Part* p2=particles[i2];
-    if(particles[i1]->fitness(current_ub) < particles[i2]->fitness(current_ub)){
-      p1=particles[i2];
-      p2=particles[i1];
-    }
-    double r = (2.0*rand())/RAND_MAX;
-    Vector h = p2->position + r*(p1->position-p2->position);
+		if(tree->search(h)){
+		  Part p(h);
+		  if (p.fitness(current_ub) > p2->fitness(current_ub)){
+			 *p2=p;
+		//	 cout << i1 << "," << i2 << endl;
+		//	 cout << "improved:" << -p2->fitness(current_ub) << endl;
+		   }
+		}
 
-    if(tree->search(h)){
-      Part p(h);
-      if (p.fitness(current_ub) > p2->fitness(current_ub)){
-         *p2=p;
-    //   cout << -p2->fitness(current_ub) << endl;
-       }
-    }
+		Part* pbest=particles[0];
+		for(int i=1;i<popsize; i++)
+			if(particles[i]->fitness(current_ub) > pbest->fitness(current_ub))
+				pbest=particles[i];
 
-    Cell* aux;
-    if (p1->fitness(current_ub) > p2->fitness(current_ub))
-       aux=tree->search(p1->position);
-    else
-       aux=tree->search(p2->position);
-
-
+		Cell* aux=tree->search(pbest->position);
 		last_node = aux;
 		return aux;
+
 	}
 }
