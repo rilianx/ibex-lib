b70fb3cdb35611f6a2b95ee1d6130523e6092bd0
victor <victor@Legend>
2018-11-08 07:57:33 -0300

    last changes

--- plugins/optim/src/loup/ibex_LoupFinderTrustRegion.cpp
+++ plugins/optim/src/loup/ibex_LoupFinderTrustRegion.cpp
@@ -47,91 +47,67 @@ std::pair<IntervalVector, double> LoupFinderTrustRegion::find(const IntervalVect
 			else throw NotFound();
 		} catch(NotFound&) { }
 
-		if (!found) throw NotFound();
-
+		if (!found){
+			throw NotFound();
+		}
 	 	IntervalVector box_aux(box.size());
-	 	while ((old_ub.second-new_ub.second>1e-6) || (flag)){
-	 		if (old_ub.second-new_ub.second < 1e-6) flag = false;
+	 	box_aux = box;
+	 	while ((old_ub.second-p.second>1e-6) || (flag)){
+	 		if (old_ub.second-p.second < 1e-6) flag = false;
 	 		else flag = true;
 		 	line_search = true;
-	 		Vector old_exp = new_ub.first.mid();
+	 		Vector old_exp = p.first.mid();
 	 		for (int i = 0 ; i < box.size() ; i++){
-	 			if ((std::abs(box[i].lb()-new_ub.first[i].lb())<1e-6) || (std::abs(box[i].ub()-new_ub.first[i].ub())<1e-6)){
+	 			if ((std::abs(box[i].lb()-p.first[i].lb())<1e-6) || (std::abs(box[i].ub()-p.first[i].ub())<1e-6)){
 	 				pp = 1;
 	 			}
 	 			else{
 	 				pp = 1;
 	 			}
-				if ((old_exp[i]-(alpha/pp)*box[i].diam()>=initial_box[i].lb()) && (old_exp[i]+(alpha/pp)*box[i].diam()<=initial_box[i].ub()))
-					box_aux[i] = Interval(old_exp[i]-(alpha/pp)*box[i].diam(),old_exp[i]+(alpha/pp)*box[i].diam());
-				else if ((old_exp[i]-(alpha/pp)*box[i].diam()>=initial_box[i].lb()) && (old_exp[i]+(alpha/pp)*box[i].diam()>initial_box[i].ub()))
-					box_aux[i] = Interval(old_exp[i]-(alpha/pp)*box[i].diam(),initial_box[i].ub());
-				else if ((old_exp[i]-(alpha/pp)*box[i].diam()<initial_box[i].lb()) && (old_exp[i]+(alpha/pp)*box[i].diam()<=initial_box[i].ub())){
-					box_aux[i] = Interval(initial_box[i].lb(),old_exp[i]+(alpha/pp)*box[i].diam());
-					line_search=false;
-				}
-				else if ((old_exp[i]-alpha*box[i].diam()<initial_box[i].lb()) && (old_exp[i]+alpha*box[i].diam()>initial_box[i].ub())){
+				if ((old_exp[i]-(alpha/pp)*box_aux[i].diam()/2>=initial_box[i].lb()) && (old_exp[i]+(alpha/pp)*box_aux[i].diam()/2<=initial_box[i].ub()))
+					box_aux[i] = Interval(old_exp[i]-(alpha/pp)*box_aux[i].diam()/2,old_exp[i]+(alpha/pp)*box_aux[i].diam()/2);
+				else if ((old_exp[i]-(alpha/pp)*box_aux[i].diam()/2>=initial_box[i].lb()) && (old_exp[i]+(alpha/pp)*box_aux[i].diam()/2>initial_box[i].ub()))
+					box_aux[i] = Interval(old_exp[i]-(alpha/pp)*box_aux[i].diam()/2,initial_box[i].ub());
+				else if ((old_exp[i]-(alpha/pp)*box_aux[i].diam()/2<initial_box[i].lb()) && (old_exp[i]+(alpha/pp)*box_aux[i].diam()/2<=initial_box[i].ub()))
+					box_aux[i] = Interval(initial_box[i].lb(),old_exp[i]+(alpha/pp)*box_aux[i].diam()/2);
+				else if ((old_exp[i]-alpha*box_aux[i].diam()/2<initial_box[i].lb()) && (old_exp[i]+alpha*box_aux[i].diam()/2>initial_box[i].ub()))
 					box_aux[i] = Interval(initial_box[i].lb(),initial_box[i].ub());
-					line_search=false;
-				}
 	 		}
+		 	old_ub = p;
 	 		try {
-	 			pair<IntervalVector,double> newnew_ub = make_pair(old_loup_point, old_loup);
- 		 		old_ub = new_ub;
-
-//	 			if (line_search){
-//	 				newnew_ub=finder_abs_taylor.find(box_aux,box_aux.mid(),new_ub.second);
-//		 			newnew_ub=finder_abs_taylor.find(box_aux,new_ub.first.mid(),new_ub.second);
-//					pair<double, double> c1_t1 = make_pair(POS_INFINITY,0);
-//					IntervalVector point_a = old_ub.first;
-//					PFunction pf(*f_goal,  newnew_ub.first.mid(),new_ub.first.mid());
-//					c1_t1 = pf.optimize(Interval(POS_INFINITY), PFunction::MIN, POS_INFINITY,  0.1);
-//
-//					if (f_goal->eval(point_a+c1_t1.second*(newnew_ub.first-point_a)).ub() < newnew_ub.second){
-//						newnew_ub.first = point_a+c1_t1.second*(newnew_ub.first-point_a);
-//						newnew_ub.second = f_goal->eval(old_loup_point).ub();
-//					}
-//	 			}
-//	 			else
-		 		newnew_ub=finder_abs_taylor.find(box_aux,box_aux.mid(),new_ub.second);
-
-	 			if(newnew_ub.second < new_ub.second){
-	 		 		new_ub = newnew_ub;
+		 		new_ub=finder_abs_taylor.find(box_aux,p.first.mid(),p.second);
+//				pair<double, double> c1_t1 = make_pair(POS_INFINITY,0);
+//				IntervalVector point_a = old_ub.first;
+//				PFunction pf(*f_goal,  p.first.mid(),new_ub.first.mid());
+//				c1_t1 = pf.optimize(Interval(POS_INFINITY), PFunction::MIN, POS_INFINITY,  0.1);
+//				if (f_goal->eval(point_a+c1_t1.second*(new_ub.first-point_a)).ub() < new_ub.second){
+//					new_ub.first = point_a+c1_t1.second*(new_ub.first-point_a);
+//					new_ub.second = f_goal->eval(old_loup_point).ub();
+//				}
+	 			if(new_ub.second < p.second){
 	 		 		p = new_ub;
 	 		 	}
-	 		}
-	 		catch(NotFound&) {}
+	 		} catch(NotFound&) {}
 	 		try {
-				pair<IntervalVector,double> newnew_ub = make_pair(old_loup_point, old_loup);
-				old_ub = new_ub;
+				new_ub=finder_xt.find(box_aux,p.first.mid(),p.second);
+//				pair<double, double> c1_t1 = make_pair(POS_INFINITY,0);
+//				IntervalVector point_a = old_ub.first;
+//				PFunction pf(*f_goal,  p.first.mid(),new_ub.first.mid());
+//				c1_t1 = pf.optimize(Interval(POS_INFINITY), PFunction::MIN, POS_INFINITY,  0.1);
+//				if (f_goal->eval(point_a+c1_t1.second*(new_ub.first-point_a)).ub() < new_ub.second){
+//					new_ub.first = point_a+c1_t1.second*(new_ub.first-point_a);
+//					new_ub.second = f_goal->eval(old_loup_point).ub();
+//				}
+	 			if(new_ub.second < p.second){
+	 		 		p = new_ub;
+	 		 	}
 
-//	 			if (line_search){
-//	 				newnew_ub=finder_abs_taylor.find(box_aux,box_aux.mid(),new_ub.second);
-//		 			newnew_ub=finder_abs_taylor.find(box_aux,new_ub.first.mid(),new_ub.second);
-//					pair<double, double> c1_t1 = make_pair(POS_INFINITY,0);
-//					IntervalVector point_a = old_ub.first;
-//					PFunction pf(*f_goal,  newnew_ub.first.mid(),new_ub.first.mid());
-//					c1_t1 = pf.optimize(Interval(POS_INFINITY), PFunction::MIN, POS_INFINITY,  0.1);
-//
-//					if (f_goal->eval(point_a+c1_t1.second*(newnew_ub.first-point_a)).ub() < newnew_ub.second){
-//						newnew_ub.first = point_a+c1_t1.second*(newnew_ub.first-point_a);
-//						newnew_ub.second = f_goal->eval(old_loup_point).ub();
-//					}
-//	 			}
-//	 			else
-				newnew_ub=finder_xt.find(box_aux,box_aux.mid(),new_ub.second);
+			} catch(NotFound&) {}
 
-				if(newnew_ub.second < new_ub.second){
-					new_ub = newnew_ub;
-					p = new_ub;
-				}
-			}
-			catch(NotFound&) {}
- 			if (!flag)
- 				break;
 	 	}
-	 	if (found)
+	 	if (found){
 	 		return p;
+	 	}
 	 	else
 	 		throw NotFound();
 	 }
