6d13d4ba96dc1377bbf474ec9889001c1dd748c0
Your Name <you@example.com>
2025-05-28 17:54:48 -0400

    conv changed

--- src/data/ibex_Cov.cpp
+++ src/data/ibex_Cov.cpp
@@ -14,6 +14,7 @@
 #include <cassert>
 #include <string.h>
 
+
 using namespace std;
 
 namespace ibex {
--- src/data/ibex_Cov.h
+++ src/data/ibex_Cov.h
@@ -17,6 +17,8 @@
 #include <vector>
 #include <fstream>
 #include <stack>
+#include <cstdint>
+
 
 namespace ibex {
 
